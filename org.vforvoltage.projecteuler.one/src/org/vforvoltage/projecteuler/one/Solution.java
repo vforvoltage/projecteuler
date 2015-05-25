aa9d4df620723e406cb67cc6acc667d9a2c090b1
James Simmons <jmsimm@gmail.com>
2015-05-25 17:43:08 -0400

    adding source for first two solutions

--- org.vforvoltage.projecteuler.one/src/org/vforvoltage/projecteuler/one/Solution.java
+++ org.vforvoltage.projecteuler.one/src/org/vforvoltage/projecteuler/one/Solution.java
@@ -1,5 +1,28 @@
 package org.vforvoltage.projecteuler.one;
 
+import java.util.Scanner;
+
 public class Solution {
 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		long t, n;
+		long sum;
+		t = in.nextInt();
+		for (int i = 0; i < t; i++) {
+			n = in.nextInt();
+			sum = calcSum(n);
+			System.out.println(sum);
+		}
+	}
+
+	public static long calcSum(long n) {
+		long result = sumDiv(3, n - 1) + sumDiv(5, n - 1) - sumDiv(15, n - 1);
+		return result;
+	}
+
+	public static long sumDiv(long n, long p) {
+		long x = (long) n * (p / n) * ((p / n) + 1) / 2;
+		return x;
+	}
 }
--- org.vforvoltage.projecteuler.two/src/org/vforvoltage/projecteuler/two/Solution.java
+++ org.vforvoltage.projecteuler.two/src/org/vforvoltage/projecteuler/two/Solution.java
@@ -1,5 +1,36 @@
 package org.vforvoltage.projecteuler.two;
 
+import java.util.Scanner;
+
 public class Solution {
 
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t;
+		t = in.nextInt();
+
+		long[] fibArray = new long[86];
+		fibArray[0] = 0;
+		fibArray[1] = 1;
+		for (int i = 2; i < fibArray.length; i++) {
+			fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+		}
+
+		for (int i = 0; i < t; i++) {
+			long n = in.nextLong();
+			long sum = 0;
+
+			int index = 0;
+			while (fibArray[index] < n) {
+				index++;
+			}
+			index--;
+			while (index % 3 != 0) {
+				index--;
+			}
+
+			sum = (fibArray[index + 2] - 1) / 2;
+			System.out.println(sum);
+		}
+	}
 }
